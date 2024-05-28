c2bb4bf76bd9869f8ebd06931d9623d1f220ceba
K.H.W <etr5032322@gmail.com>
2024-05-28 10:27:29 +0800

    first project

---
+++ src/hw1/HW1.java
@@ -0,0 +1,10 @@
+package hw1;
+
+public class HW1 {
+	public static void main(String[] args) {
+		System.out.println(6 + 12);
+		System.out.println(6 * 12);
+		
+		System.out.println("== master ==");
+	}
+}
